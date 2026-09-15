edea94d076dbf03f8baea4b7a57286474f7437a6
nazmulhossenshanto <https://github.com/nazmulhossenshanto>
2026-09-15 12:35:49 +0600

    added product delete method

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -5,9 +7,56 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json());
 
+
+// connect to mongo db
+const uri = "mongodb+srv://smart_deals_users_db:HlUGPxm6csupqNX6@cluster0.pnssve1.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+}); 
+
+
 app.get('/', (req, res )=>{
     res.send('Your server is ok')
 });
+
+async function run (){
+  try{
+    await client.connect();
+     await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      // create product database into mongo db
+      const db = client.db('smart_db');
+      const productsCollection = db.collection('products');
+
+
+
+
+      // create product into db
+      app.post('/products', async(req, res)=>{
+        const newUser = req.body;
+        const result = await productsCollection.insertOne(newUser);
+        console.log( 'product from server', result);
+        res.send(result)
+      });
+      // delete product from db
+      app.delete('/products/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await productsCollection.deleteOne(query);
+        res.send(result)
+      })
+      
+
+  }
+  finally{
+
+  }
+}
+run().catch(console.dir)
 
 app.listen(port, ()=>{
     console.log(`server is running on port : ${port}`);
