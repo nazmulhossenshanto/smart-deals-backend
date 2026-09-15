561bb1d1bfbdf01cd01a95493ee4ea710c373cac
nazmulhossenshanto <https://github.com/nazmulhossenshanto>
2026-09-15 09:10:43 +0600

    initial server setup done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res )=>{
+    res.send('Your server is ok')
+});
+
+app.listen(port, ()=>{
+    console.log(`server is running on port : ${port}`);
+})
